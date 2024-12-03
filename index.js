43fa2aef02b83de390749e69421ca47e2ed5e9ea
subrata jana <digititanic@gmail.com>
2024-12-03 19:33:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+//index.js
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+	e.preventDefault();
+	console.log(this);
+	emailjs.sendForm('service_dmcekjp','template_akz2w71',this).then(function(result){
+		alert('send okk');
+		console.log(result.text);
+		document.getElementById('contact-form').reset();
+	}, function(error){
+		alert("that one not oke");
+		console.log(error.text);
+
+	});
+});
